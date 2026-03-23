a0ae219fd7a118563f6531bf992f39627a71ae8a
Dasun-Herath <www.476dassa@gmail.com>
2026-03-24 01:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,10 +102,14 @@ if st.session_state.user:
         st.bar_chart(df.set_index("Subjects"))
 
         # Save to Firebase
-        db.child("students").push({
-            "name": name,
-            "average": avg
-        })
+        try:
+    db.child("students").push({
+        "name": name,
+        "average": avg
+    })
+    st.success("Saved to Firebase ✅")
+except Exception as e:
+    st.error(e)
 
     # ================= AI CHATBOT =================
     st.subheader("🤖 AI Study Assistant")
